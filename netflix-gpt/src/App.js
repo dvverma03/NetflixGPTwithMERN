8165f7e33d987b7e94dde5adb5d7d8537be0eaa4
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-07-08 00:56:04 +0530

    Update App.js

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import Body from './components/Body';
 import { Provider } from 'react-redux';
 import appStore from './utils/appStore';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    window.alert("If you are getting dangerous site error please use different browser or you can open with details button and only visit")
+  },[])
   return (
     <div className="App">
       <Provider store={appStore}><Body/></Provider>
